17479c57eb83365275753ae27422788757982c1c
justnikee <hk5561111212@gmail.com>
2023-03-27 00:29:23 +0530

    user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,33 +3,52 @@ import Head from "next/head";
 import { SignIn, SignInButton, useUser, SignOutButton } from '@clerk/nextjs'
 import { api } from '~/utils/api'
 
+const CreatePostWizard = () => {
+  const { user } = useUser();
+  if (!user) return null;
+
+  console.log(user)
+  return (
+    <div className="flex gap-4 w-full">
+      <img src={user?.profileImageUrl} alt="profile image" className="w-14 h-14 rounded-full
+      "/>
+      <input placeholder="type some emoji's" className="bg-transparent grow outline-none" />
+    </div>
+  )
+}
+
 const Home: NextPage = () => {
 
   const user = useUser()
-  const { data } = api.posts.getAll.useQuery();
+  const { data, isLoading } = api.posts.getAll.useQuery();
+
+  if (isLoading) return <div>Loading...</div>
+  if (!data) return <div>Something Went Wrong.</div>
 
   return (
     <>
       <Head>
         <title>Created with T3 Stack</title>
       </Head>
-      <section className="flex justify-center items-center bg-green-200 h-screen w-screen">
-        <div className="text-center">
-          <h1 className="text-xl from-stone-900 bold">Sign Up here</h1>
-          <p className="bg-blue-300 px-8 py-2 flex items-center justify-center text-center my-4">
-            {!user.isSignedIn && <SignInButton />}
-            {!!user.isSignedIn && <SignOutButton />}
-          </p>
-        </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-        <div>
-          <div>
-            {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+      <main className="flex justify-center h-screen">
+        <div className="w-full border-x border-slate-200 md:max-w-2xl ">
+          <div className="border-b border-slate-200 py-4">
+            <p className="">
+              {!user.isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
+              {user.isSignedIn && <CreatePostWizard />}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            {[...data, ...data]?.map((post) => (<div className="p-8 border-b border-slate-200" key={post.id}>{post.content}</div>))}
           </div>
         </div>
-      </section>
+      </main>
     </>
   );
 };
+
+
+
+
 
 export default Home;
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,33 @@
-import { z } from "zod";
 
+import { z } from "zod";
+import { User } from '@clerk/nextjs/dist/api'
+import { clerkClient } from "@clerk/nextjs/server"
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+    return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
+}
+
 export const postsRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.post.findMany();
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        const posts = await ctx.prisma.post.findMany({
+            take: 100,
+        });
+
+
+        const users = (
+            await clerkClient.users.getUserList({
+                userId: posts.map((post) => post.autherId),
+                limit: 100,
+            })
+        ).map(filterUserForClient)
+
+        console.log(users)
+
+        return posts.map((post) => ({
+            post,
+            auther: users.find((user) => user.id === post.autherId),
+        }));
+
     }),
 });
